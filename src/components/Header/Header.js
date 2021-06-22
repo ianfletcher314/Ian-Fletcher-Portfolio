7795f51ae75f782eae09b0dba768070634061683
Ian Fletcher <ianfletcher314@gmail.com>
2021-06-22 12:30:19 -0500

    added style to header component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,7 @@
 // header section will have my name in upper left and nav section upper right. nav sectin will render 
 // About, Clients, and Projects, and Contact Componenets 
 import React from "react";
+import "./style.css";
 
 
 function Header() {
