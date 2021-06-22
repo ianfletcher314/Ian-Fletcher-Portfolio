d581b27568847c1cf6389e222cdf9e815a18f92a
Ian Fletcher <ianfletcher314@gmail.com>
2021-06-22 13:08:56 -0500

    contact animation now working on buttons

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -9,14 +9,14 @@ function Contact() {
         <h2 className="Contactheader center cyan-text text-accent-3">Contact</h2>
 
 <section id="contact" className="container caveat-bold center space pads">
-    <a className="center grey-text text-accent-3" href="mailto:ianfletcher314@gmail.comcom">EMAIL</a>
-    <a className="center grey-text text-accent-3" href="images/IanFletcher-Resume.pdf" target="_blank"
+    <a className="center grey-text text-accent-3 contact" href="mailto:ianfletcher314@gmail.comcom">EMAIL</a>
+    <a className="center grey-text text-accent-3 contact" href="images/IanFletcher-Resume.pdf" target="_blank"
         download>RESUME</a>
 
-    <a className="center grey-text text-accent-3" href="https://www.linkedin.com/in/ian-fletcher-a51420124/"
+    <a className="center grey-text text-accent-3 contact" href="https://www.linkedin.com/in/ian-fletcher-a51420124/"
         target="_blank">LINKEDIN</a>
-    <a className="center grey-text text-accent-3" href="https://github.com/ianfletcher314" target="_blank"
-        class="ContactGit">GITHUB</a>
+    <a className="center grey-text text-accent-3 contact" href="https://github.com/ianfletcher314" target="_blank"
+        class="center grey-text text-accent-3">GITHUB</a>
 </section>
       </div>
       );
