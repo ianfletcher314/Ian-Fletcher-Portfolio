2afaae3b833d999860389b5bbaf4d989f2838038
Ian Fletcher <ianfletcher314@gmail.com>
2021-06-23 16:45:43 -0500

    info array of ProjectContainer now has the 6 projects I want to display updated with correect photos, URLs, and Names. Now I need to bring in the html from the old portfolio's smaller 2 projects and extraolate it to fit 6 projects.

--- src/components/ProjectContainer/ProjectContainer.js
+++ src/components/ProjectContainer/ProjectContainer.js
@@ -1,13 +1,47 @@
 import React from "react"
 import Projects from "../Projects/Projects"
 import kiskisphoto from  "../../images/Project2.png"
-
+import horoscope from  "../../images/Project3.PNG"
+import budget from  "../../images/basicbudget.png"
+import directory from  "../../images/directory.png"
+import calendar from  "../../images/calendar.png"
+import fitness from  "../../images/fitness.png"
 
 function ProjectContainer(){
     const info = [{
         appName : "kiskis",
         url : "https://lit-escarpment-01189.herokuapp.com/",
         photo: <img src={kiskisphoto}></img>
+
+    },
+    {
+        appName : "Basic Budget",
+        url : "https://basicbudget.herokuapp.com/",
+        photo: <img src={budget}></img>
+
+    },
+    {
+        appName : "Horoscope Finder",
+        url : "https://samuel6roth.github.io/HoroscopeFinder/",
+        photo: <img src={horoscope}></img>
+
+    },
+    {
+        appName : "Company Directory",
+        url : "https://ianfletcher314.github.io/company-directory/",
+        photo: <img src={directory}></img>
+
+    },
+    {
+        appName : "Daily Calendar",
+        url : "https://ianfletcher314.github.io/DailyCalendar/",
+        photo: <img src={calendar}></img>
+
+    },
+    {
+        appName : "Fitness Tracker",
+        url : "https://fitnesstrackerianfletcher.herokuapp.com/",
+        photo: <img src={fitness}></img>
 
     }
     ]
