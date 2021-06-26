4be43280e00957567ab0e1ab47aa328b22154747
Ian Fletcher <ianfletcher314@gmail.com>
2021-06-26 17:25:01 -0500

    imported jumbotron photo so it can be deployed

--- src/components/Jumbotron/Jumbotron.js
+++ src/components/Jumbotron/Jumbotron.js
@@ -1,11 +1,12 @@
 import React from "react";
 import "./style.css";
+import image from "../../images/ian2.jpg"
 
 function Jumbotron() {
     return (
         <div>
       <div class="container center">
-        <img src="images/ian2.jpg" alt="Ian's Photo" class="container circle responsive-img hide-on-med-and-down"></img>
+        <img src={image} alt="Ian's Photo" class="container circle responsive-img hide-on-med-and-down"></img>
     </div>
       </div>
       );
