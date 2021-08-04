43c81a30a7165757d9341d0b7a5220dbf53e89d8
Ian Fletcher <ianfletcher314@gmail.com>
2021-08-04 14:48:36 -0500

    made the projects in a row instead of a collumn now I need to get rid of the boarder in the div and put it directly onto the image and make sure they are all the same size and have a little padding. I think 2 rows of 3 may be the best option.

--- src/components/ProjectContainer/ProjectContainer.js
+++ src/components/ProjectContainer/ProjectContainer.js
@@ -13,42 +13,42 @@ function ProjectContainer() {
         appName: "kiskis",
         url: "https://lit-escarpment-01189.herokuapp.com/",
         photo: <img src={kiskisphoto} alt="photo" id="project2"
-        class="col s6   responsive-img hoverable pads bradius layer btnbtn"></img>
+        className="responsive-img"></img>
 
     },
     {
         appName: "Basic Budget",
         url: "https://basicbudget.herokuapp.com/",
         photo: <img src={budget} alt="photo" id="project2"
-        class="col s6   responsive-img hoverable pads bradius layer btnbtn"></img>
+        className="responsive-img"></img>
 
     },
     {
         appName: "Horoscope Finder",
         url: "https://samuel6roth.github.io/HoroscopeFinder/",
         photo: <img src={horoscope} alt="photo" id="project2"
-        class="col s6   responsive-img hoverable pads bradius layer btnbtn"></img>
+        className="responsive-img"></img>
 
     },
     {
         appName: "Company Directory",
         url: "https://ianfletcher314.github.io/company-directory/",
         photo: <img src={directory} alt="photo" id="project2"
-        class="col s6   responsive-img hoverable pads bradius layer btnbtn"></img>
+        className="responsive-img "></img>
 
     },
     {
         appName: "Daily Calendar",
         url: "https://ianfletcher314.github.io/DailyCalendar/",
         photo: <img src={calendar} alt="photo" id="project2"
-        class="col s6   responsive-img hoverable pads bradius layer btnbtn"></img>
+        className="responsive-img"></img>
 
     },
     {
         appName: "Fitness Tracker",
         url: "https://fitnesstrackerianfletcher.herokuapp.com/",
         photo: <img src={fitness} alt="photo" id="project2"
-        class="col s6   responsive-img hoverable pads bradius layer btnbtn"></img>
+        className="responsive-img"></img>
 
     }
     ]
@@ -57,13 +57,13 @@ function ProjectContainer() {
         <div>
             <h2 className="center pink-text text-accent-1" id="projects">PROJECTS</h2>
             <div>
-                <section className="container caveat-bold projectsContainer center">
+                <div className="caveat-bold projectsContainer center row">
                     {info.map(project => (
-                        <div class="space">
-                            <Projects project={project} className="center"/>
-                        </div>
+                       
+                            <Projects project={project} />
+                       
                     ))}
-                </section>
+                </div>
             </div>
 
         </div>
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,12 +1,13 @@
 // Projects will be conditionally rendered through Header as a child. 
 // Projects will hold the links to a project I ahve and will be used multiple times on the project page 
 import React from "react";
+import "./style.css";
 
 function Projects ({project}) {
     return(
-        <div className="center">
-        <h1 className= "center">{project.appName}</h1>
-        <a href={project.url} >
+        <div className="hoverable bradius btnbtn col s1 layer">
+        {/* <h1 className= "center">{project.appName}</h1> */}
+        <a  href={project.url} >
         {project.photo}
         </a>
        
