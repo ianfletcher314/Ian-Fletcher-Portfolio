53eaa2388019e1206e381fa57be169b5cff6b4e4
Ian Fletcher <ianfletcher314@gmail.com>
2021-08-18 13:14:26 -0500

    chagned wording of about section. and full functionality of projects is done. Now I have to make the descriptions accurate for each project

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -14,7 +14,7 @@ function About() {
                 <p className="aboutMeText caveat flow-text grey-text text-darken-3 ">
                     I am a full stack developer located in the St. Louis area specializing in Web Design and maintenance. After
                     years of working in the music industry both on and off stage I realized it was time to make a change so I
-                    enrolled in a coding bootcamp in early 2021. With the goal of making a professional looking websites for my
+                    enrolled in a 24-week coding intensive at Washington University in early 2021. With the goal of making a professional looking websites for my
                     own musical ventures, I quickly grew a passion for creating portfolio pages for the artists I know and love.
                     I like to get to know my clients a little bit before designing for them so please feel free to contact me
                     bellow so we can discuss your ideas!
