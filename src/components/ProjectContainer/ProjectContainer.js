83ff9d3934b83e8f62a68927b581dff2bb7a87b2
Ian Fletcher <ianfletcher314@gmail.com>
2021-09-09 13:19:13 -0500

    deleted unused info constant from previous build

--- src/components/ProjectContainer/ProjectContainer.js
+++ src/components/ProjectContainer/ProjectContainer.js
@@ -10,48 +10,7 @@ import calendar from "../../images/happyhealth.png"
 import "./style.css";
 
 function ProjectContainer() {
-    // const info = [{
-    //     appName: "kiskis",
-    //     url: "https://lit-escarpment-01189.herokuapp.com/",
-    //     photo: <img src={kiskisphoto} alt="photo" id="project2"
-    //         className="responsive-img hoverable bradius btnbtn "></img>,
-    //     para: "An Application designed to store your data securely!"
-
-    // },
-    // {
-    //     appName: "Basic Budget",
-    //     url: "https://basicbudget.herokuapp.com/",
-    //     photo: <img src={budget} alt="photo" id="project2"
-    //         className="responsive-img hoverable bradius btnbtn "></img>,
-    //     para: "An Application designed to help budget yoru money!"
-
-    // },
-    // {
-    //     appName: "Horoscope Finder",
-    //     url: "https://samuel6roth.github.io/HoroscopeFinder/",
-    //     photo: <img src={horoscope} alt="photo" id="project2"
-    //         className="responsive-img hoverable bradius btnbtn "></img>,
-    //     para: "An Application designed to give you your daily horoscope!"
-
-    // },
-    // {
-    //     appName: "Directory",
-    //     url: "https://ianfletcher314.github.io/company-directory/",
-    //     photo: <img src={directory} alt="photo" id="project2"
-    //         className="responsive-img hoverable bradius btnbtn "></img>,
-    //     para: "An Application designed to display a company directory generated randomly!"
-
-    // },
-    // {
-    //     appName: "Daily Calendar",
-    //     url: "https://ianfletcher314.github.io/DailyCalendar/",
-    //     photo: <img src={calendar} alt="photo" id="project2"
-    //         className="responsive-img hoverable bradius btnbtn "></img>,
-    //     para: "An Application designed to give you a daily meeting calendar!"
-
-    // },
-  
-    // ]
+   
     // This is where the scroll animation is handled!
     const handleMouseOverKis = (event) => {
         console.log("KisKis");
