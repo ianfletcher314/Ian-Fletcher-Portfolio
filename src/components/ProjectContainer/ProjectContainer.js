37f9c230a57f16a159f7007eb1834a9413237ab8
Ian Fletcher <ianfletcher314@gmail.com>
2021-09-09 13:18:03 -0500

    deleted unused photos from images library

--- src/components/ProjectContainer/ProjectContainer.js
+++ src/components/ProjectContainer/ProjectContainer.js
@@ -6,59 +6,52 @@ import horoscope from "../../images/Project33.PNG"
 import budget from "../../images/basicbudget.png"
 import directory from "../../images/directory.png"
 import calendar from "../../images/happyhealth.png"
-import fitness from "../../images/fitness.png"
+
 import "./style.css";
 
 function ProjectContainer() {
-    const info = [{
-        appName: "kiskis",
-        url: "https://lit-escarpment-01189.herokuapp.com/",
-        photo: <img src={kiskisphoto} alt="photo" id="project2"
-            className="responsive-img hoverable bradius btnbtn "></img>,
-        para: "An Application designed to store your data securely!"
+    // const info = [{
+    //     appName: "kiskis",
+    //     url: "https://lit-escarpment-01189.herokuapp.com/",
+    //     photo: <img src={kiskisphoto} alt="photo" id="project2"
+    //         className="responsive-img hoverable bradius btnbtn "></img>,
+    //     para: "An Application designed to store your data securely!"
 
-    },
-    {
-        appName: "Basic Budget",
-        url: "https://basicbudget.herokuapp.com/",
-        photo: <img src={budget} alt="photo" id="project2"
-            className="responsive-img hoverable bradius btnbtn "></img>,
-        para: "An Application designed to help budget yoru money!"
+    // },
+    // {
+    //     appName: "Basic Budget",
+    //     url: "https://basicbudget.herokuapp.com/",
+    //     photo: <img src={budget} alt="photo" id="project2"
+    //         className="responsive-img hoverable bradius btnbtn "></img>,
+    //     para: "An Application designed to help budget yoru money!"
 
-    },
-    {
-        appName: "Horoscope Finder",
-        url: "https://samuel6roth.github.io/HoroscopeFinder/",
-        photo: <img src={horoscope} alt="photo" id="project2"
-            className="responsive-img hoverable bradius btnbtn "></img>,
-        para: "An Application designed to give you your daily horoscope!"
+    // },
+    // {
+    //     appName: "Horoscope Finder",
+    //     url: "https://samuel6roth.github.io/HoroscopeFinder/",
+    //     photo: <img src={horoscope} alt="photo" id="project2"
+    //         className="responsive-img hoverable bradius btnbtn "></img>,
+    //     para: "An Application designed to give you your daily horoscope!"
 
-    },
-    {
-        appName: "Directory",
-        url: "https://ianfletcher314.github.io/company-directory/",
-        photo: <img src={directory} alt="photo" id="project2"
-            className="responsive-img hoverable bradius btnbtn "></img>,
-        para: "An Application designed to display a company directory generated randomly!"
+    // },
+    // {
+    //     appName: "Directory",
+    //     url: "https://ianfletcher314.github.io/company-directory/",
+    //     photo: <img src={directory} alt="photo" id="project2"
+    //         className="responsive-img hoverable bradius btnbtn "></img>,
+    //     para: "An Application designed to display a company directory generated randomly!"
 
-    },
-    {
-        appName: "Daily Calendar",
-        url: "https://ianfletcher314.github.io/DailyCalendar/",
-        photo: <img src={calendar} alt="photo" id="project2"
-            className="responsive-img hoverable bradius btnbtn "></img>,
-        para: "An Application designed to give you a daily meeting calendar!"
+    // },
+    // {
+    //     appName: "Daily Calendar",
+    //     url: "https://ianfletcher314.github.io/DailyCalendar/",
+    //     photo: <img src={calendar} alt="photo" id="project2"
+    //         className="responsive-img hoverable bradius btnbtn "></img>,
+    //     para: "An Application designed to give you a daily meeting calendar!"
 
-    },
-    {
-        appName: "Fitness Tracker",
-        url: "https://fitnesstrackerianfletcher.herokuapp.com/",
-        photo: <img src={fitness} alt="photo" id="project2"
-            className="responsive-img hoverable bradius btnbtn "></img>,
-        para: "An Application designed to track your Fitness"
-
-    }
-    ]
+    // },
+  
+    // ]
     // This is where the scroll animation is handled!
     const handleMouseOverKis = (event) => {
         console.log("KisKis");
