663bd84e6c3dca95eb409b6dfa870b4a9da5d792
Ian Fletcher <ianfletcher314@gmail.com>
2021-10-06 12:04:11 -0500

    deleted floating fab because it is not nececary and detracts from the simplictity of the page

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -42,17 +42,7 @@ function About() {
                 </p>
 
             </section>
-            {/* <div class="fixed-action-btn" onMouseEnter={handleMouseOverContactBtn}>
-                <a class="btn-floating btn-large green accent-2 " >
-                    <i class="large material-icons">mode_edit</i>
-                </a>
-                <ul>
-                    <li><a class="btn-floating red"><i class="material-icons">insert_chart</i></a></li>
-                    <li><a class="btn-floating yellow darken-1"><i class="material-icons">format_quote</i></a></li>
-                    <li><a class="btn-floating green"><i class="material-icons">publish</i></a></li>
-                    <li><a class="btn-floating blue"><i class="material-icons">attach_file</i></a></li>
-                </ul>
-            </div> */}
+ 
             
             
 
