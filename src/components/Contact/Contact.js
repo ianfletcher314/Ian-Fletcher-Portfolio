00f5498e180d03634e6b8db5c6155f74b2f4063f
Ian Fletcher <ianfletcher314@gmail.com>
2021-10-06 12:25:56 -0500

    cleaned up code on contact.js

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -11,28 +11,24 @@ import resume from "../../images/IanFletcherDevelopmentResume.pdf"
 function Contact() {
     return (
         <div>
-        <h3 className="Contactheader center cyan-text text-accent-3 Header">CONTACT ME</h3>
+            <h3 className="Contactheader center cyan-text text-accent-3 Header">CONTACT ME</h3>
 
             <section id="contact" className="container caveat-bold center space roof">
-            <a className="center grey-text text-accent-3 contact" href="https://github.com/ianfletcher314" target="_blank"
-                    >GITHUB
-                    <img src={git} className="responsive-img icon"></img></a>
-
-                
-
-                <a className="center grey-text text-accent-3 contact" href={resume} target="_blank"
-                    download>RESUME
-                    <img src={doc} className="responsive-img icon"></img></a>
-
-                    <a className="center grey-text text-accent-3 contact" href="mailto:ianfletcher314@gmail.comcom">
-                    EMAIL<img src={mail} className="responsive-img iconicon"></img>
+                <a className="center grey-text text-accent-3 contact" href="https://github.com/ianfletcher314" target="_blank">
+                GITHUB<img src={git} className="responsive-img icon"></img>
                 </a>
 
-                <a className="center grey-text text-accent-3 contact" href="https://www.linkedin.com/in/ian-fletcher-a51420124/"
-                    target="_blank">LINKEDIN
-                    <img src={link} className="responsive-img icon" ></img></a>
+                <a className="center grey-text text-accent-3 contact" href={resume} target="_blank"  download>
+                RESUME<img src={doc} className="responsive-img icon"></img>
+                </a>
 
-                
+                <a className="center grey-text text-accent-3 contact" href="mailto:ianfletcher314@gmail.comcom">
+                EMAIL<img src={mail} className="responsive-img iconicon"></img>
+                </a>
+
+                <a className="center grey-text text-accent-3 contact" href="https://www.linkedin.com/in/ian-fletcher-a51420124/"  target="_blank">
+                LINKEDIN<img src={link} className="responsive-img icon" ></img>
+                </a>
             </section>
         </div>
     );
