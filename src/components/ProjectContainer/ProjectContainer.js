b02ea9f56faeb198488645c6340bcc7ec5be8872
Ian Fletcher <ianfletcher314@gmail.com>
2021-10-07 11:58:30 -0500

    comented and organized project container component

--- src/components/ProjectContainer/ProjectContainer.js
+++ src/components/ProjectContainer/ProjectContainer.js
@@ -6,8 +6,8 @@ import calendar from "../../images/happyhealth.png"
 import "./style.css";
 
 function ProjectContainer() {
-   
-    // This is where the scroll animation is handled!
+
+    // scroll animation for KisKis
     const handleMouseOverKis = (event) => {
         console.log("KisKis");
         var elem = document.getElementById('kiskispara');
@@ -30,73 +30,6 @@ function ProjectContainer() {
         var projects = document.getElementById('projects');
         projects.classList.add("hidden")
     };
-    const handleMouseOverHor = (event) => {
-        console.log("Horoscope");
-        var elem = document.getElementById('horoscopepara');
-        elem.classList.remove("hidden")
-        var p1 = document.getElementById('project2');
-        var p2 = document.getElementById('project1');
-        var p3 = document.getElementById('project3');
-        var p4 = document.getElementById('project4');
-        var name = document.getElementById('HoroscopeName');
-        var link = document.getElementById('horlink');
-        link.classList.add("center")
-        name.classList.add("hidden")
-        p1.classList.remove("col")
-        p1.classList.remove("s6")
-        p1.classList.remove("layer")
-        p1.classList.add("center")
-        p2.classList.add("hidden")
-        p3.classList.add("hidden")
-        p4.classList.add("hidden")
-        var projects = document.getElementById('projects');
-        projects.classList.add("hidden")
-    };
-    const handleMouseOverBud = (event) => {
-        console.log("Budget");
-        var elem = document.getElementById('budgetpara');
-        elem.classList.remove("hidden")
-        var p1 = document.getElementById('project3');
-        var p2 = document.getElementById('project2');
-        var p3 = document.getElementById('project1');
-        var p4 = document.getElementById('project4');
-        var name = document.getElementById('BudgetName');
-        var link = document.getElementById('budlink');
-        link.classList.add("center")
-        name.classList.add("hidden")
-        p1.classList.remove("col")
-        p1.classList.remove("s6")
-        p1.classList.remove("layer")
-        p1.classList.add("center")
-        p2.classList.add("hidden")
-        p3.classList.add("hidden")
-        p4.classList.add("hidden")
-        var projects = document.getElementById('projects');
-        projects.classList.add("hidden")
-    };
-    const handleMouseOverCal = (event) => {
-        console.log("Calendar");
-        var elem = document.getElementById('calendarpara');
-        elem.classList.remove("hidden")
-        var p1 = document.getElementById('project4');
-        var p2 = document.getElementById('project2');
-        var p3 = document.getElementById('project3');
-        var p4 = document.getElementById('project1');
-        var name = document.getElementById('CalName');
-        var link = document.getElementById('haplink');
-        link.classList.add("center")
-        name.classList.add("hidden")
-        p1.classList.remove("col")
-        p1.classList.remove("s6")
-        p1.classList.remove("layer")
-        p1.classList.add("center")
-        p2.classList.add("hidden")
-        p3.classList.add("hidden")
-        p4.classList.add("hidden")
-        var projects = document.getElementById('projects');
-        projects.classList.add("hidden")
-    };
-
     const handleMouseLeaveKis = () => {
         console.log("KisKis");
         var elem = document.getElementById('kiskispara');
@@ -119,6 +52,29 @@ function ProjectContainer() {
         var projects = document.getElementById('projects');
         projects.classList.remove("hidden")
     }
+    // scroll animation for Horoscope Finder
+    const handleMouseOverHor = (event) => {
+        console.log("Horoscope");
+        var elem = document.getElementById('horoscopepara');
+        elem.classList.remove("hidden")
+        var p1 = document.getElementById('project2');
+        var p2 = document.getElementById('project1');
+        var p3 = document.getElementById('project3');
+        var p4 = document.getElementById('project4');
+        var name = document.getElementById('HoroscopeName');
+        var link = document.getElementById('horlink');
+        link.classList.add("center")
+        name.classList.add("hidden")
+        p1.classList.remove("col")
+        p1.classList.remove("s6")
+        p1.classList.remove("layer")
+        p1.classList.add("center")
+        p2.classList.add("hidden")
+        p3.classList.add("hidden")
+        p4.classList.add("hidden")
+        var projects = document.getElementById('projects');
+        projects.classList.add("hidden")
+    };
     const handleMouseLeaveHor = (event) => {
         console.log("Horoscope");
         var elem = document.getElementById('horoscopepara');
@@ -141,6 +97,29 @@ function ProjectContainer() {
         var projects = document.getElementById('projects');
         projects.classList.remove("hidden")
     };
+    // scroll animation for Basic Budget
+    const handleMouseOverBud = (event) => {
+        console.log("Budget");
+        var elem = document.getElementById('budgetpara');
+        elem.classList.remove("hidden")
+        var p1 = document.getElementById('project3');
+        var p2 = document.getElementById('project2');
+        var p3 = document.getElementById('project1');
+        var p4 = document.getElementById('project4');
+        var name = document.getElementById('BudgetName');
+        var link = document.getElementById('budlink');
+        link.classList.add("center")
+        name.classList.add("hidden")
+        p1.classList.remove("col")
+        p1.classList.remove("s6")
+        p1.classList.remove("layer")
+        p1.classList.add("center")
+        p2.classList.add("hidden")
+        p3.classList.add("hidden")
+        p4.classList.add("hidden")
+        var projects = document.getElementById('projects');
+        projects.classList.add("hidden")
+    };
     const handleMouseLeaveBud = (event) => {
         console.log("Budget");
         var elem = document.getElementById('budgetpara');
@@ -162,6 +141,29 @@ function ProjectContainer() {
         p4.classList.remove("hidden")
         var projects = document.getElementById('projects');
         projects.classList.remove("hidden")
+    };
+    // scroll animation for HappyHealth
+    const handleMouseOverCal = (event) => {
+        console.log("Calendar");
+        var elem = document.getElementById('calendarpara');
+        elem.classList.remove("hidden")
+        var p1 = document.getElementById('project4');
+        var p2 = document.getElementById('project2');
+        var p3 = document.getElementById('project3');
+        var p4 = document.getElementById('project1');
+        var name = document.getElementById('CalName');
+        var link = document.getElementById('haplink');
+        link.classList.add("center")
+        name.classList.add("hidden")
+        p1.classList.remove("col")
+        p1.classList.remove("s6")
+        p1.classList.remove("layer")
+        p1.classList.add("center")
+        p2.classList.add("hidden")
+        p3.classList.add("hidden")
+        p4.classList.add("hidden")
+        var projects = document.getElementById('projects');
+        projects.classList.add("hidden")
     };
     const handleMouseLeaveCal = (event) => {
         console.log("Calendar");
@@ -186,13 +188,13 @@ function ProjectContainer() {
         projects.classList.remove("hidden")
     };
 
-
+    // This is where the HTML for the component goes to be rendered
     return (
+        // HEADER
         <div className="container">
             <h3 className="center pink-text text-accent-1 Header " id="projects">PROJECTS</h3>
-
             <div className="caveat-bold projectsContainer center row" >
-                {/* this is where the projects go */}
+                {/* Happy Health --------------------*/}
                 <div className="col s6 layer" id="project4"
                     onMouseEnter={handleMouseOverCal} onMouseLeave={handleMouseLeaveCal}>
                     <a href="https://happyhealth404.herokuapp.com/">
@@ -203,14 +205,14 @@ function ProjectContainer() {
                         <h4>HappyHealth</h4>
                         <p>Provides multiple healthy meal options based upon the number of calories the user wants to consume.</p>
                         <h6>Mongo DB / Express / React / Node Js / Bcrypt / Material UI / Connect-Mongo</h6>
-                        <a className="nonlink " href="https://github.com/ianfletcher314/happyhealth">GITHUB</a> 
-                        <p className="noheight"></p> 
+                        <a className="nonlink " href="https://github.com/ianfletcher314/happyhealth">GITHUB</a>
+                        <p className="noheight"></p>
                         <a className="nonlink " href="https://happyhealth404.herokuapp.com/"> DEPLOYED SITE</a>
 
                     </div>
                     <h5 className="center flow-text" id="CalName">HappyHealth</h5>
                 </div>
-
+                {/* KisKis ----------------------*/}
                 <div className="col s6 layer" id="project1"
                     onMouseEnter={handleMouseOverKis} onMouseLeave={handleMouseLeaveKis}>
                     <a href="https://lit-escarpment-01189.herokuapp.com/">
@@ -222,32 +224,30 @@ function ProjectContainer() {
                         <h4>KisKis</h4>
                         <p>Stores any personal information securely in your own personal encrypted password and information hub.</p>
                         <h6>Node Js / Express-Handlebars / MySqul / Cryptr / Materialize / CSS</h6>
-                        <a className="nonlink " href="https://github.com/ianfletcher314/kiskis">GITHUB</a> 
-                        <p className="noheight"></p> 
+                        <a className="nonlink " href="https://github.com/ianfletcher314/kiskis">GITHUB</a>
+                        <p className="noheight"></p>
                         <a className="nonlink " href="https://lit-escarpment-01189.herokuapp.com/"> DEPLOYED SITE</a>
                     </div>
                     <h5 className="center flow-text" id="KisKissName">KisKis</h5>
                 </div>
-
+                {/* Horoscope Finder --------------------*/}
                 <div className="col s6 layer" id="project2"
                     onMouseEnter={handleMouseOverHor} onMouseLeave={handleMouseLeaveHor}>
-
                     <a href="https://samuel6roth.github.io/HoroscopeFinder/">
                         <img src={horoscope} alt="photo"
                             className="responsive-img hoverable bradius btnbtn smallest " id="horlink"></img>
                     </a>
-
                     <div className="hidden" id="horoscopepara">
                         <h4>Horoscope Finder</h4>
                         <p>Displays the user’s daily horoscope and an inspirational quote depending on their zodiac sign.</p>
                         <h6> HTML / CSS / JS / Moment.js / Materialize / Aztro API / Daily Quotes API</h6>
-                        <a className="nonlink " href="https://github.com/ianfletcher314/HoroscopeFinder">GITHUB</a> 
-                        <p className="noheight"></p> 
+                        <a className="nonlink " href="https://github.com/ianfletcher314/HoroscopeFinder">GITHUB</a>
+                        <p className="noheight"></p>
                         <a className="nonlink " href="https://samuel6roth.github.io/HoroscopeFinder/"> DEPLOYED SITE</a>
                     </div>
                     <h5 className="center flow-text" id="HoroscopeName">Horoscope Finder</h5>
                 </div>
-
+                {/* Basic Budget --------------------*/}
                 <div className="col s6 layer" id="project3"
                     onMouseEnter={handleMouseOverBud} onMouseLeave={handleMouseLeaveBud}>
                     <a href="https://basicbudget.herokuapp.com/">
@@ -259,8 +259,8 @@ function ProjectContainer() {
                         <h4>Basic Budget</h4>
                         <p>Tracks a user’s income and spending in an easy to read chart with online and offline data storage.</p>
                         <h6>HTML / CSS / Node JS / Mongo DB / Indexed DB</h6>
-                        <a className="nonlink " href="https://github.com/ianfletcher314/BasicBudget">GITHUB</a> 
-                        <p className="noheight"></p> 
+                        <a className="nonlink " href="https://github.com/ianfletcher314/BasicBudget">GITHUB</a>
+                        <p className="noheight"></p>
                         <a className="nonlink " href="https://basicbudget.herokuapp.com/"> DEPLOYED SITE</a>
                     </div>
                     <h5 className="center flow-text" id="BudgetName">Basic Budget</h5>
