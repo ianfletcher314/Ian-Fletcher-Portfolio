ac46756803c040412166badd4baab9af1aae3a26
Ian Fletcher <ianfletcher314@gmail.com>
2022-01-13 15:06:14 -0600

    took caveat out of about section because it was not being used and now messed up the spacing

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,34 +1,43 @@
 // About will be a child of header and will be conditionally rendered when About is clicked in the Nav Section
 import React from "react";
 import "./style.css";
-import ian from "../../images/Ian5.jpg"
+import ian from "../../images/Ian5.jpg";
 
 function About() {
-    return (
-        <div className="row ">
-            <h3 className="Header center purple-text text-lighten-3 col s12 " id="about">ABOUT ME</h3>
-            <img className="col s4 push-s1 responsive-image pic z hide-on-med-and-down" src={ian}></img>
-            <section className="container flow-text caveat-bold aboutContainer col s6 push-s1 hide-on-med-and-down ">
-
-                <p className="aboutMeText caveat grey-text text-darken-3 ">
-                    I am a full stack developer located in the St. Louis area specializing in Web Design and Sales. With the goal of making a professional looking websites for my
-                    own musical ventures, I quickly grew a passion for building applications and learning new skills.
-                    I like to get to know my clients a little bit before designing for them so please feel free to contact me
-                    bellow so we can discuss your ideas!
-                </p>
-
-            </section>
-            <section className="container center flow-text caveat-bold aboutContainer col s8 push-s2 hide-on-large-only show-on-medium-and-down ">
-
-                <p className="aboutMeText caveat grey-text text-darken-3 ">
-                    I am a full stack developer located in the St. Louis area specializing in Web Design and Sales. With the goal of making a professional looking websites for my
-                    own musical ventures, I quickly grew a passion for building applications and learning new skills.
-                    I like to get to know my clients a little bit before designing for them so please feel free to contact me
-                    bellow so we can discuss your ideas!
-                </p>
-            </section>
-        </div>
-    );
+  return (
+    <div className="row ">
+      <h3
+        className="Header center purple-text text-lighten-3 col s12 "
+        id="about"
+      >
+        ABOUT ME
+      </h3>
+      <img
+        className="col s4 push-s1 responsive-image pic z hide-on-med-and-down"
+        src={ian}
+      ></img>
+      <section className="container flow-text aboutContainer col s6 push-s1 hide-on-med-and-down ">
+        <p className="aboutMeText grey-text text-darken-3 ">
+          I am a full stack developer located in the St. Louis area specializing
+          in Web Design and Sales. With the goal of making a professional
+          looking websites for my own musical ventures, I quickly grew a passion
+          for building applications and learning new skills. I like to get to
+          know my clients a little bit before designing for them so please feel
+          free to contact me bellow so we can discuss your ideas!
+        </p>
+      </section>
+      <section className="container center flow-text aboutContainer col s8 push-s2 hide-on-large-only show-on-medium-and-down ">
+        <p className="aboutMeText  grey-text text-darken-3 ">
+          I am a full stack developer located in the St. Louis area specializing
+          in Web Design and Sales. With the goal of making a professional
+          looking websites for my own musical ventures, I quickly grew a passion
+          for building applications and learning new skills. I like to get to
+          know my clients a little bit before designing for them so please feel
+          free to contact me bellow so we can discuss your ideas!
+        </p>
+      </section>
+    </div>
+  );
 }
 
 export default About;
